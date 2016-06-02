716c4d6a0d616e353127af97bd1fe985469cda0f
TailC <wlxlj24@qq.com>
2016-06-02 16:39:08 +0800

    bluetooth method

--- CoreBluetoothDemo/CoreBluetoothDemo/ViewController.m
+++ CoreBluetoothDemo/CoreBluetoothDemo/ViewController.m
@@ -11,7 +11,8 @@
 
 @interface ViewController ()
 <
-CBCentralManagerDelegate
+CBCentralManagerDelegate,
+CBPeripheralDelegate
 >
 
 /** 系统蓝牙设备管理对象，可以把他理解为主设备，通过他，可以去扫描和链接外设  */
@@ -81,6 +82,7 @@ CBCentralManagerDelegate
 }
 
 -(void)centralManager:(CBCentralManager *)central didDiscoverPeripheral:(CBPeripheral *)peripheral advertisementData:(NSDictionary<NSString *,id> *)advertisementData RSSI:(NSNumber *)RSSI{
+	
 	NSLog(@"当扫描到设备:%@",peripheral.name);
 	//接下来可以连接设备
 	//接下连接我们的测试设备，如果你没有设备，可以下载一个app叫lightbule的app去模拟一个设备
@@ -104,6 +106,11 @@ CBCentralManagerDelegate
 //连接到Peripherals-成功
 - (void)centralManager:(CBCentralManager *)central didConnectPeripheral:(CBPeripheral *)peripheral{
 	NSLog(@">>>连接到名称为（%@）的设备-成功",peripheral.name);
+	//设置的peripheral委托CBPeripheralDelegate
+	//@interface ViewController : UIViewController<CBCentralManagerDelegate,CBPeripheralDelegate>
+	[peripheral setDelegate:self];
+	//扫描外设Services，成功后会进入方法：-(void)peripheral:(CBPeripheral *)peripheral didDiscoverServices:(NSError *)error{
+	[peripheral discoverServices:nil];
 }
 
 //连接到Peripherals-失败
@@ -116,8 +123,136 @@ CBCentralManagerDelegate
 	NSLog(@">>>外设连接断开连接 %@: %@\n", [peripheral name], [error localizedDescription]);
 }
 
+//扫描到Services
+-(void)peripheral:(CBPeripheral *)peripheral didDiscoverServices:(NSError *)error{
+	//  NSLog(@">>>扫描到服务：%@",peripheral.services);
+	if (error)
+	{
+		NSLog(@">>>Discovered services for %@ with error: %@", peripheral.name, [error localizedDescription]);
+		return;
+	}
+	
+	for (CBService *service in peripheral.services) {
+		NSLog(@"%@",service.UUID);
+		//扫描每个service的Characteristics，扫描到后会进入方法： -(void)peripheral:(CBPeripheral *)peripheral didDiscoverCharacteristicsForService:(CBService *)service error:(NSError *)error
+		[peripheral discoverCharacteristics:nil forService:service];
+	}
+	
+}
 
+//扫描到Characteristics
+-(void)peripheral:(CBPeripheral *)peripheral didDiscoverCharacteristicsForService:(CBService *)service error:(NSError *)error{
+	if (error)
+	{
+		NSLog(@"error Discovered characteristics for %@ with error: %@", service.UUID, [error localizedDescription]);
+		return;
+	}
+	
+	for (CBCharacteristic *characteristic in service.characteristics)
+	{
+		NSLog(@"service:%@ 的 Characteristic: %@",service.UUID,characteristic.UUID);
+	}
+	
+	//获取Characteristic的值，读到数据会进入方法：-(void)peripheral:(CBPeripheral *)peripheral didUpdateValueForCharacteristic:(CBCharacteristic *)characteristic error:(NSError *)error
+	for (CBCharacteristic *characteristic in service.characteristics){
+		{
+			[peripheral readValueForCharacteristic:characteristic];
+		}
+	}
+	
+	//搜索Characteristic的Descriptors，读到数据会进入方法：-(void)peripheral:(CBPeripheral *)peripheral didDiscoverDescriptorsForCharacteristic:(CBCharacteristic *)characteristic error:(NSError *)error
+	for (CBCharacteristic *characteristic in service.characteristics){
+		[peripheral discoverDescriptorsForCharacteristic:characteristic];
+	}
+	
+	
+}
 
+//获取的charateristic的值
+-(void)peripheral:(CBPeripheral *)peripheral didUpdateValueForCharacteristic:(CBCharacteristic *)characteristic error:(NSError *)error{
+	//打印出characteristic的UUID和值
+	//!注意，value的类型是NSData，具体开发时，会根据外设协议制定的方式去解析数据
+	NSLog(@"characteristic uuid:%@  value:%@",characteristic.UUID,characteristic.value);
+	
+}
+
+//搜索到Characteristic的Descriptors
+-(void)peripheral:(CBPeripheral *)peripheral didDiscoverDescriptorsForCharacteristic:(CBCharacteristic *)characteristic error:(NSError *)error{
+	
+	//打印出Characteristic和他的Descriptors
+	NSLog(@"characteristic uuid:%@",characteristic.UUID);
+	for (CBDescriptor *d in characteristic.descriptors) {
+		NSLog(@"Descriptor uuid:%@",d.UUID);
+	}
+	
+}
+//获取到Descriptors的值
+-(void)peripheral:(CBPeripheral *)peripheral didUpdateValueForDescriptor:(CBDescriptor *)descriptor error:(NSError *)error{
+	//打印出DescriptorsUUID 和value
+	//这个descriptor都是对于characteristic的描述，一般都是字符串，所以这里我们转换成字符串去解析
+	NSLog(@"characteristic uuid:%@  value:%@",[NSString stringWithFormat:@"%@",descriptor.UUID],descriptor.value);
+}
+
+//写数据
+-(void)writeCharacteristic:(CBPeripheral *)peripheral
+			characteristic:(CBCharacteristic *)characteristic
+					 value:(NSData *)value{
+	
+	//打印出 characteristic 的权限，可以看到有很多种，这是一个NS_OPTIONS，就是可以同时用于好几个值，常见的有read，write，notify，indicate，知知道这几个基本就够用了，前连个是读写权限，后两个都是通知，两种不同的通知方式。
+	/*
+	 typedef NS_OPTIONS(NSUInteger, CBCharacteristicProperties) {
+	 CBCharacteristicPropertyBroadcast												= 0x01,
+	 CBCharacteristicPropertyRead													= 0x02,
+	 CBCharacteristicPropertyWriteWithoutResponse									= 0x04,
+	 CBCharacteristicPropertyWrite													= 0x08,
+	 CBCharacteristicPropertyNotify													= 0x10,
+	 CBCharacteristicPropertyIndicate												= 0x20,
+	 CBCharacteristicPropertyAuthenticatedSignedWrites								= 0x40,
+	 CBCharacteristicPropertyExtendedProperties										= 0x80,
+	 CBCharacteristicPropertyNotifyEncryptionRequired NS_ENUM_AVAILABLE(NA, 6_0)		= 0x100,
+	 CBCharacteristicPropertyIndicateEncryptionRequired NS_ENUM_AVAILABLE(NA, 6_0)	= 0x200
+	 };
+	 
+	 */
+	NSLog(@"%lu", (unsigned long)characteristic.properties);
+	
+	
+	//只有 characteristic.properties 有write的权限才可以写
+	if(characteristic.properties & CBCharacteristicPropertyWrite){
+		/*
+		 最好一个type参数可以为CBCharacteristicWriteWithResponse或type:CBCharacteristicWriteWithResponse,区别是是否会有反馈
+		 */
+		[peripheral writeValue:value forCharacteristic:characteristic type:CBCharacteristicWriteWithResponse];
+	}else{
+		NSLog(@"该字段不可写！");
+	}
+	
+	
+}
+
+//设置通知
+-(void)notifyCharacteristic:(CBPeripheral *)peripheral
+			 characteristic:(CBCharacteristic *)characteristic{
+	//设置通知，数据通知会进入：didUpdateValueForCharacteristic方法
+	[peripheral setNotifyValue:YES forCharacteristic:characteristic];
+	
+}
+
+//取消通知
+-(void)cancelNotifyCharacteristic:(CBPeripheral *)peripheral
+				   characteristic:(CBCharacteristic *)characteristic{
+	
+	[peripheral setNotifyValue:NO forCharacteristic:characteristic];
+}
+
+//停止扫描并断开连接
+-(void)disconnectPeripheral:(CBCentralManager *)centralManager
+				 peripheral:(CBPeripheral *)peripheral{
+	//停止扫描
+	[centralManager stopScan];
+	//断开连接
+	[centralManager cancelPeripheralConnection:peripheral];
+}
 
 
 
